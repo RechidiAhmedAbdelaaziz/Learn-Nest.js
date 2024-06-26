682fdb0b4c9e77b56967d89e1342f6fcd0fe754b
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-06-26 14:35:25 +0100

    make the first route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class UserController {
+    @Get('users')
+    getAll(): string[] {
+        return ['ahmed',];
+    }
 
 }
